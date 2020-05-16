99882530153bc5fff710e94c9fbbd597d02b4b1c
niluxweb <niluxweb@gmail.com>
2020-05-15 23:24:23 -0400

    Modificamos la ruta del SW

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@ const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    '/',
-    '/index.html',
+    //'/',
+    'index.html',
     'css/style.css',
     'img/favicon.ico',
     'img/avatars/hulk.jpg',
